ff1dca8465ee1f0549154e670f5bb8fe15916476
SolamaeteanTehCoder <143941545+SolamateanTehCoder@users.noreply.github.com>
2025-08-24 17:19:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amusic',
-    version='2.0.4',
+    version='2.1.0',
     packages=find_packages(),
     install_requires=[
         'mido',
